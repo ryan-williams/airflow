310703ee106b2de9dba1ab79ab8981505bcf7a3d
Ryan Williams <ryan.blake.williams@gmail.com>
2019-07-10 03:51:54 +0000

    use bootstrap link glyph, add arrows to readme screenshot puppeteering

--- airflow/contrib/example_dags/kubeflow_pipelines_puppeteer.js
+++ airflow/contrib/example_dags/kubeflow_pipelines_puppeteer.js
@@ -13,15 +13,25 @@ const browser = await puppeteer.launch({
   args: [
     '--no-sandbox',
   ],
-  userDataDir: process.env("USER_DATA_DIR"),
+  userDataDir: process.env["USER_DATA_DIR"],
 });
 const page = await browser.newPage();
 
-let width = 1300, height = 470;
+let width = 1500, height = 600;
 
+async function wait(s) {
+  console.log("sleep " + s + "s");
+  return page.waitFor(s * 1000)
+}
+
+const log = console.log;
+
+idx = 0;
 async function screenshot(name) {
-  await page.screenshot({path: dir + '/' + name + '.png'});
-  console.log("screenshotted:", name);
+  idx += 1;
+  await page.screenshot({path: dir + '/' + (('0' + idx).slice(-2)) + '-' + name + '.png'});
+  log("screenshotted:", name);
+  //await wait(1);
 }
 
 async function resize() {
@@ -47,7 +57,7 @@ async function resize() {
     windowId
   });
 
-  await page.waitFor(1000);
+  await wait(1);
 }
 
 let statusIdxs = {};
@@ -62,7 +72,7 @@ async function statusCount(row, label) {
   }
   return page.evaluate(
     ([row, idx]) => {
-    const text = $('#dags tr:nth-child(' + row + ') > td:nth-child(6) > svg > g:nth-child(' + idx + ') > text').text();
+      const text = $('#dags tr:nth-child(' + row + ') > td:nth-child(6) > svg > g:nth-child(' + idx + ') > text').text();
       if (text === '') return 0;
       return parseInt(text);
     },
@@ -76,29 +86,100 @@ if (!AIRFLOW_HOST) {
 }
 const homepage = AIRFLOW_HOST + '/admin/';
 
-async function wait(s) {
-  console.log("sleep " + s + "s");
-  return page.waitFor(s * 1000)
-}
-
-const log = console.log;
-
 async function goto(url) {
   await page.goto(url, { waitUntil: 'networkidle2' });
 }
 
-async function runCoin() {
+async function findDagRowIdx(name) {
+  return await page.evaluate(
+    (name) => {
+      return $('#dags tr > td:nth-child(3):contains(' + name + ')').parent().index();
+    },
+    name
+  );
+}
 
+async function pos(selector) {
+  return await page.evaluate((selector) => {
+    var elem = $(selector);
+    let w = elem.outerWidth(), h = elem.outerHeight();
+    if (w === 0 && h === 0) {
+      const rect = elem[0].getBoundingClientRect();
+      w = rect.width;
+      h = rect.height;
+      console.log("read svg w/h:", w, h);
+    }
+    return [ elem.offset(), { w, h } ];
+  }, selector);
+}
+
+async function arrow(selector, degrees, adjustPad) {
+  const [ offset, size ] = await pos(selector);
+  console.log("offset:", offset, "size:", size);
+  const { top, left } = offset;
+  const { w, h } = size;
+  const center = {
+    top: top + h / 2,
+    left: left + w / 2
+  };
+  const pad = Math.sqrt((w*w + h*h) / 2);
+  adjustPad = adjustPad || { x: 0, y: 0 }
+  return drawArrow(center, degrees, { x: pad + adjustPad.x, y: pad + adjustPad.y });
+}
+
+const ARROW_URL = "https://cl.ly/c1865a257057/600px-Red_arrow_southeast.svg.png";
+async function drawArrow(pos, degrees, pad) {
+  degrees = degrees || 0;
+  console.log("drawing arrow: ", pos, degrees, pad);
+  degrees = 135 - degrees;
+  pad = pad || { x: 0, y: 0 };
+  const w = 100, h = 100;
+  return await page.evaluate(([pos, w, h, degrees, pad, ARROW_URL]) => {
+    var img = document.createElement('img');
+    img.src = ARROW_URL;
+    img.width = "100";
+    img.height = "100";
+    const { top, left, right, bottom, ...rest } = pos;
+    const box = { top, left, right, bottom };
+    Object.assign(
+      img.style,
+      {
+        position: "absolute",
+        "z-index": 1000,
+        "transform": "translate(" + (-w/2) + "px," + (-h/2) + "px) rotate(" + degrees + "deg) translate(" + (-w/2 - pad.x) + "px," + (-h/2 - pad.y) + "px)",
+      },
+      rest,
+      Object.keys(box).reduce((o, k) => { o[k] = box[k] + 'px'; return o; }, {} )
+    );
+    document.body.appendChild(img);
+    return img;
+  }, [pos, w, h, degrees, pad, ARROW_URL]);
+}
+
+async function home() {
   await goto(homepage);
   await resize();
   await screenshot('homepage');
+}
 
-  const trigger = '#dags tr:nth-child(2) > td:last-child > a:first-child > span';
+async function runCoin() {
+  const dagRowIdx = await findDagRowIdx('kubeflow_pipelines_coin_example') + 1;
+  const row = '#dags tr:nth-child(' + dagRowIdx + ')';
+
+  await hoverClickTrigger(row);
+  await hoverRunning(row);
+  await waitForSuccessCircle(dagRowIdx);
+  return row;
+}
+
+async function hoverClickTrigger(row) {
+  const trigger = row + ' > td:last-child > a:first-child > span';
 
   await page.hover(trigger);
-  log('hovered');
+  log('hovered over trigger DAG button');
   await wait(.5);
-
+  await arrow(trigger, -150);
+  await wait(.5);
   await screenshot('trigger');
 
   await page.click(trigger);
@@ -111,33 +192,40 @@ async function runCoin() {
   // });
   // await page.keyboard.press('Enter');
 
-  height = 530;
+  //height += 60;
   await resize(width, height);
   await wait(1);
+}
 
-  await page.hover('#dags tr:nth-child(2) > td:nth-child(8) > svg > g:nth-child(2) > circle');
+async function hoverRunning(row) {
+  const selector = row + ' > td:nth-child(8) > svg > g:nth-child(2) > circle';
+  await page.hover(selector);
   log("hover circle");
   await wait(.5);
-
+  await arrow(selector, 90);
+  const [ { top, left }, { w, h } ] = await pos('.alert.alert-info');
+  await drawArrow( { top: top + h, left: 200 }, -90);
+  await wait(.5);
   await screenshot('running');
+}
 
+async function waitForSuccessCircle(row) {
   while (true) {
-    await wait(4000);
+    await wait(4);
     log("slept 4s");
     await page.reload();
     await wait(1);
-    log("reloaded");
-    const [ successes, pendings ] = await page.evaluate(async() => {
-      const $ = window.$;
-      const successes = await statusCount(2, 'success'); // $('#dags tr:nth-child(2) > td:nth-child(6) > svg > g:first-child:has(> text:not(:empty)) > circle');
-      const pendings = [
+    log("reloaded; checking dag row idx ", row);
+    const successes = await statusCount(row, 'success');
+    const pendings = await Promise.all(
+      [
         'running',
         'queued',
         'scheduled',
       ]
-      .map(async s => await statusCount(2, s));
-      return [ successes, pendings ];
-    });
+      .map(async s => await statusCount(row, s))
+    );
+
     log(successes, "successes, pendings:", pendings);
     if (successes > 0 && pendings.reduce((a, b) => a+b, 0) === 0) {
       break;
@@ -146,8 +234,13 @@ async function runCoin() {
     }
   }
 
-  let selector = '#dags tr:nth-child(2) > td:nth-child(6) > svg > g:first-child > circle';
+}
+
+async function clickSuccessCircle(row) {
+  const selector = row + ' > td:nth-child(6) > svg > g:first-child > circle';
   await page.hover(selector);
+  await wait(.5);
+  await arrow(selector, -45, { x: -10, y: -10 });
   await wait(.5);
   await screenshot('succeeded');
 
@@ -156,8 +249,35 @@ async function runCoin() {
   await wait(1);
 }
 
-async function clickLogs() {
-  await page.click('.table-responsive > table > tbody > tr:first-child > td:last-child > a');
+async function clickLogs(name) {
+  if (name) {
+    await goto(AIRFLOW_HOST + '/admin/taskinstance/?flt0_dag_id_equals=' + name);
+  }
+  const selector = '.table-responsive > table > tbody > tr:first-child > td:last-child > a';
+  await page.hover(selector);
+  await wait(.5);
+  await arrow(selector, 150);
+  await wait(.5);
+  await screenshot("logs-link");
+  await page.click(selector);
+}
+
+async function logsPage() {
+  // await wait(2);
+  // await page.evaluate(async () => {
+  //   console.log("scroll to:", document.body.scrollHeight);
+  //   window.scrollTo(0, document.body.scrollHeight);
+  // });
+  // await wait(1);
+  await drawArrow({ bottom: -10, left: 1070, position: "fixed" },  90);
+  await drawArrow({ bottom: 200, left: 1200, position: "fixed" }, 270);
+  await wait(1);
+  await page.evaluate(async () => {
+    console.log("scroll to:", document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+  await wait(1);
+  screenshot("logs-page");
 }
 
 async function jobUrl() {
@@ -176,6 +296,7 @@ async function gotoJob() {
   const job_url = await jobUrl();
   log("Got job URL:", job_url);
   await goto(job_url);
+  await screenshot('kfp-job-ui');
 }
 
 async function recents(dag, state) {
@@ -202,8 +323,28 @@ async function runDetails() {
   await screenshot('kfp');
 }
 
-//runCoin();
-//runDetails();
+async function homeToKFP() {
+  const dropdown = '#admin-navbar-collapse > .navbar-nav > li:nth-child(3)'
+  await page.click(dropdown + ' > a');
+  log("clicked 'Browse' menu");
+  await wait(1);
+
+  const kfp = dropdown + ' > ul > li:last-child > a';
+  await page.hover(kfp);
+  await screenshot('kfp-menu');
+
+  await page.click(kfp);
+  await wait(1);
+}
+
+async function kfpPage(load) {
+  if (load) {
+    await goto('/admin/kfp/');
+  }
+  await screenshot('kfp-page');
+  await page.click('tr:first-child > td.col-value > a');
+  await wait(1);
+}
 
 async function sequentialDetails() {
   await goto(homepage);
@@ -236,30 +377,29 @@ async function sequentialDetails() {
   await sequentialKFPJob();
 }
 
-async function sequentialKFPJob() {
-  //await recents('kubeflow_pipelines_sequential_example');
-
-  await clickLogs();
-  await wait(2);
-  await gotoJob();
+async function kfpSequentialJobPage() {
   width = 1200; height = 700; await resize();
   const graphNode = '.graphNode > div:first-child';
   await page.waitForSelector(graphNode, 10000);
-  const [ clicked, texts ] = await page.evaluate(async (graphNode) => {
-    const divs = document.querySelectorAll(graphNode);
-    let texts = [];
-    let clicked = false;
-    for (var i = 0; i < divs.length; i++) {
-      const div = divs[i];
-      const text = div.innerText;
-      texts.push(text);
-      if (text == 'echo') {
-        div.click();
-        clicked = true;
-      }
-    }
-    return [ clicked, texts ]
-  }, graphNode);
+  const [ clicked, texts ] =
+    await page.evaluate(
+      async (graphNode) => {
+        const divs = document.querySelectorAll(graphNode);
+        let texts = [];
+        let clicked = false;
+        for (var i = 0; i < divs.length; i++) {
+          const div = divs[i];
+          const text = div.innerText;
+          texts.push(text);
+          if (text == 'echo') {
+            div.click();
+            clicked = true;
+          }
+        }
+        return [ clicked, texts ]
+      },
+      graphNode
+    );
 
   console.log("Clicked graph node:", clicked, texts);
   await wait(2);
@@ -272,8 +412,35 @@ async function sequentialKFPJob() {
   await screenshot('kfp');
 }
 
-await sequentialDetails();
+async function sequentialKFPJob() {
+  //await recents('kubeflow_pipelines_sequential_example');
+
+  await clickLogs();
+  await wait(2);
+  await gotoJob();
+  await kfpSequentialJobPage();
+}
+
+
+await home();
+// const dagRowIdx = await findDagRowIdx('kubeflow_pipelines_coin_example') + 1;
+// const row = '#dags tr:nth-child(' + dagRowIdx + ')';
+// await hoverClickTrigger(row);
+
+// let row = await runCoin();
+// await clickSuccessCircle(row);
+await clickLogs('kubeflow_pipelines_coin_example');
+await logsPage();
+
+// await runCoin();
+//runDetails();
+
+// await homeToKFP();
+// await kfpPage();
+// await kfpJobPage();
+
+// await sequentialDetails();
 // await sequentialKFPJob();
 
-await browser.close();
+//await browser.close();
 })();
